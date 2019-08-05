7ba090da2381c42630717ac4ac6227e135637a28
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-05 12:54:19 +0200

    removed temp variable this_amount in Customer.statement(), as it was redundant

--- src/Customer.java
+++ src/Customer.java
@@ -24,11 +24,8 @@ public class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while(rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			
-			thisAmount = each.getCharge();
-			
+						
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -37,8 +34,8 @@ public class Customer {
 			}
 			
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		
 		// add footer lines
